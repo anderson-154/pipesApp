3befaa71da0ff8deada3b62e0aad8c07d0b469c0
Anderson cardenas <andyguaca@gmail.com>
2023-01-16 10:27:22 -0500

    module to import and exports resources of PirmeNg to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {ButtonModule} from 'primeng/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+
+//Modulo personalizado
 
 @NgModule({
   declarations: [
@@ -11,7 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ButtonModule
+    PrimeNgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
