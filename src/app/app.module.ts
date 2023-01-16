816c7698409390f96172005a1c88c06179f18ad0
Anderson cardenas <andyguaca@gmail.com>
2023-01-16 10:44:52 -0500

    create modules to work and load the primeNg module where we need, and removed of app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { SharedModule } from './shared/shared.module';
 
 //Modulo personalizado
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PrimeNgModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
